fa11af4e888680dabec8b6dbc611bac0d9284c9b
Amogh Madan <amoghmadaan@gmail.com>
2020-09-23 10:34:14 +0530

    Made the todo page reactive

--- src/user/serializers.py
+++ src/user/serializers.py
@@ -1,3 +1,4 @@
+from django.db.utils import IntegrityError
 from rest_framework import serializers
 from .models import User
 
@@ -21,14 +22,18 @@ class UserSerializer(serializers.ModelSerializer):
 
         if validated_data['password'] != validated_data['password2']:
             raise serializers.ValidationError('Both the passwords should match')
-
-        user = User(
-            email=validated_data['email'],
-            username=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-
-        return user
+        
+        try:
+            user = User.objects.get(email=validated_data['email'])
+        except User.DoesNotExist:
+            user = User(
+                email=validated_data['email'],
+                username=validated_data['email'],
+                first_name=validated_data['first_name'],
+                last_name=validated_data['last_name'],
+            )
+            user.set_password(validated_data['password'])
+            user.save()
+            return user
+        else:
+            raise serializers.ValidationError('User already exists')
